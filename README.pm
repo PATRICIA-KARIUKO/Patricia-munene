40b483c6fceb2ffa664ee4a3cae11d53fcb507cc
PATRICIA-KARIUKO <153080354+PATRICIA-KARIUKO@users.noreply.github.com>
2023-12-06 14:38:59 +0300

    Create README.pm

---
+++ README.pm
@@ -0,0 +1,16 @@
+Patricia Kariuko portfolio
+I am a passionate Information technology developer  with a focus on creating innovative and user-friendly applications.
+Below are some of the projects i have done
+Project 1: An international college platform
+Description: Created an online college platform with features like user authentication,home,about,campus life and student enrollment form
+role: Front end developer
+Technologies used:Node.js, Firebase
+Project 2:Airbnb booking platform
+Description:Created an airbnb with features like user authentication  and secure payment intergration
+role:Front end developer
+technologies:Xamp database
+SKILLS
+Javascript(Node.js)
+HTML/CSS, php
+Database management(Firebase, xamp)
+problem solving
